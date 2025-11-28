c8f0ae78e796c633c907fe87e2bae26ff4a94985
vanhellsing102 <mdm78774@gmail.com>
2025-11-28 04:56:31 -0700

    dashboard page

--- app/libs/navbar.ts
+++ app/libs/navbar.ts
@@ -7,7 +7,8 @@ const navbarLinks: TNavbar[] = [
     { path: '/', route: 'Home' },
     { path: '/shop', route: 'Shop' },
     { path: '/contact', route: 'Contact' },
-    { path: '/about', route: 'About' }
+    { path: '/about', route: 'About' },
+    { path: '/dashboard', route: 'Dashboard' },
 ]
 
 export default navbarLinks;
